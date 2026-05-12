d2af776386d9e8c2bd80e2929a2f2e60f02caeae
Nalindu <nalindugra@gmail.com>
2026-05-12 23:49:25 +0530

    Add ShadCN

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { Button } from "./components/ui/button";
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1 className='bg-red-500'>Hi</h1>
+      <Button>Hi</Button>
     </>
   )
 }

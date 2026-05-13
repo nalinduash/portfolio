9661f0c0fa2ddaf5c5d91b12ce8d57b97e5143bc
Nalindu <nalindugra@gmail.com>
2026-05-13 10:59:34 +0530

    Add theme switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { Button } from "./components/ui/button";
 import './App.css'
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
       <Button>Hi</Button>
-    </>
+    </ThemeProvider>
   )
 }
 
